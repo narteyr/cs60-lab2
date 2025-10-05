b77944843f371c3e54aea557faae70f9c8e8275a
Tyler Schill <tyler.j.schill.27@dartmouth.edu>
2025-10-05 22:14:23 +0000

    Update server_comm.py

--- part2/server_comm.py
+++ part2/server_comm.py
@@ -2,14 +2,15 @@
 import threading
 import socket
 import enum
+import os
 
 class StatusCode(enum.Enum):
     BAD_REQUEST = 400
 
 
 
-STANDARD_METHODS = set(["GET"])
-MIN_HEADERS = 3
+ACCEPTED_METHODS = set(["GET"])
+MIN_PARAMS = 2
 
 class ServerCommunicator(threading.Thread):
     def __init__(self, server, count, client_socket: socket, client_addr):
@@ -28,6 +29,42 @@ class ServerCommunicator(threading.Thread):
                     break
 
                 request = data.decode()
+                request_params = request.split(" ")
+                if len(request_params) < MIN_PARAMS:
+                    error_message = "HTTP 400: BAD REQUEST"
+                    self.client_socket.sendall(error_message.encode())
+                    break
+                print(request_params)
+                method, pathname = request_params[0], request_params[1]
+
+                if method in ACCEPTED_METHODS and pathname.startswith("/"):
+                    base_dir = os.path.dirname(os.path.abspath(__file__))
+                    requested_path = pathname.lstrip("/")
+                    file_path = os.path.join(base_dir, requested_path)
+                    file_path = os.path.normpath(file_path)
+
+                    try :
+                        with open(file_path, 'r') as file:
+                            html_content = file.read()
+                            response = (
+                            "HTTP/1.1 200 OK\r\n"
+                            "\r\n"
+                            f"{html_content}"
+                            )
+                            self.client_socket.sendall(response.encode())
+                            break
+
+
+                    except FileNotFoundError:
+                        not_found_message = "HTTP/1.1 404 Not Found\r\n\r\n <html><head></head><body><h1>404 Not Found</h1></body></html>\r\n"
+                        self.client_socket.sendall(not_found_message.encode())
+                        break
+                else:
+                    error_message = "HTTP 400: BAD REQUEST"
+                    self.client_socket.sendall(error_message.encode())
+                    break
+
+                request = data.decode()
                 message = f"Thread{self.count}(ip_address: {self.client_addr}, request: {request})"
                 self.server.log_thread_msg(message)
 
@@ -37,14 +74,8 @@ class ServerCommunicator(threading.Thread):
                     self.client_socket.sendall(error_message.encode())
                     break
 
-                method, pathname = request[0], request[1]
-                if method not in STANDARD_METHODS or not pathname.startswith("/"):
-                    error_message: str = f"HTTP/1.1 {StatusCode.BAD_REQUEST.value} {StatusCode.BAD_REQUEST.name}\r\n\r\n"
-                    self.client_socket.sendall(error_message.encode())
-                    break
 
-
-        finally:            
+        finally:
             self.client_socket.close()
             message: str = "[-] Closing connection to " + str(self.client_addr)
             self.server.log_thread_msg(message)
